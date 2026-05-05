9f7030bce7eeacd9285a9fb9f1c712bdfff04a28
vDevopsvinod <your-email@example.com>
2026-05-05 18:40:44 +0530

    fix gpg tty issue final

--- scripts/worker.sh
+++ scripts/worker.sh
@@ -33,7 +33,7 @@ sudo apt-get install -y apt-transport-https ca-certificates curl gpg
 sudo mkdir -p /etc/apt/keyrings
 
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key \
-| sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+| sudo gpg --dearmor --batch --yes --no-tty -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /" \
 | sudo tee /etc/apt/sources.list.d/kubernetes.list
